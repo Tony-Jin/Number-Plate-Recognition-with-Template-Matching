aad25ce8e56559b34b3301c4ac9d55cb019def10
Tony-Jin <632719425@qq.com>
2019-07-25 11:25:32 +0100

    Delete midfilter.m

--- midfilter.m
+++
@@ -1,8 +0,0 @@
-function I = midfilter(noise_image)
-
-intImage = integralImage(noise_image);
-avgH = integralKernel([1 1 3 3], 1/9);
-avg_image = integralFilter(intImage, avgH);
-I = uint8(avg_image);
-
-end
